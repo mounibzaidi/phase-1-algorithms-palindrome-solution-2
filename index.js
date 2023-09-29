7820e74f6e19535e4902c4dd964d9e2a7df46c31
zaidi mounib <mounibzaidi@gmail.com>
2023-09-29 16:16:50 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 function isPalindrome(word) {
-  // Iterate from the beginning of the string to the middle of the string
+  // iterate from the beginning of the string to the middle of the string
   for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
-    // Calculate the corresponding index from the end of the string
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
     const endIndex = word.length - 1 - startIndex;
-
-    // Compare the letter we're iterating over to the corresponding letter at the end of the string
     if (word[startIndex] !== word[endIndex]) {
-      // If the letters don't match, return false
+      // if the letters don't match, return false
       return false;
     }
   }
 
-  // If we reach the middle, and all the letters match, return true
+  // if we reach the middle, and all the letters match, return true
   return true;
 }
 
